2a141d15983a41ca1ab4a55a38a5706b4a41482d
denis_jethro <denisjethro@gmail.com>
2023-10-17 11:29:32 +0700

    report small fix

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,33 +10,22 @@ class ReportController extends Controller
     // Menampilkan formulir untuk laporan keluhan pelanggan
     public function showForm()
     {
-        return view('report');
+        return view('RoomService.report');
     }
 
     // Menyimpan laporan keluhan
     public function submitReport(Request $request)
     {
         try {
-            $description = $request->input('description');
-
             $data = $request->validate([
                 'description' => 'required'
             ]);
-
-
-            // Validasi data jika diperlukan
 
             // Simpan data ke database menggunakan model Eloquent
             $report = new Report();
             $report->description = $data['description'];
             $report->id_room = 1; 
             $report->save(); 
-
-            // CARA YANG KE-3
-            $data_insert =[
-                'description' => $data['description'],
-                'id_room' => 1
-            ];
 
             return redirect()->route('show.form')->with('message' , 'Laporan Keluhan berhasil disimpan');
 
