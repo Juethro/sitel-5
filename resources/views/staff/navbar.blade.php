a04c789499a24d9288e00e64c0d7ccbb7436b4ee
Denis M Jethro <denisjethro@gmail.com>
2023-10-25 20:21:34 +0700

    Navbar staff fixed

--- resources/views/staff/navbar.blade.php
+++ resources/views/staff/navbar.blade.php
@@ -2,14 +2,14 @@
 
     <!-- Kotak Navigasi -->
     <div class="ml-2 p-4 flex items-center" >
-        <img class="w-10 h-10 rounded-full" src="images/profil_default.png">
+        <img class="w-10 h-10 rounded-full" src="{{ asset('images/profil_default.png') }}">
         <h3 class="text-sm font-medium ml-3">HRD</h3>  
     </div>
 
     <!-- Profil User -->
     <div class="mr-2 p-5 flex items-center">
         <div class="bg-white inline-block rounded" style="box-shadow: 0 8px 0px rgba(220, 66, 149, 1)">
-        <a href="#" class="font-bold px-4 py-2">Logout</a>
+        <a href="/logout" class="font-bold px-4 py-2">Logout</a>
       </div>
     </div>
 
