bc57896f6543795bca499ab87364b3236f0bfa81
Denis M Jethro <denisjethro@gmail.com>
2023-10-26 22:26:11 +0700

    Fixed Roomtype seeder

--- database/seeders/RoomTypesSeeder.php
+++ database/seeders/RoomTypesSeeder.php
@@ -15,7 +15,7 @@ class RoomTypesSeeder extends Seeder
     {
         DB::table('roomtypes')->insert([
             ['roomtype_name' => 'President Room', 'bedsize' => 'XL', 'bed_count' =>10, 'max_cap' => 5, 'price_per_night' => 1000000],
-            ['roomtype_name' => 'Deluxe Room', 'bedsize' => 'L', 'bed_count' =>10, 'max_cap' => 4, 'price_per_night' => 8000000],
+            ['roomtype_name' => 'Deluxe Room', 'bedsize' => 'L', 'bed_count' =>10, 'max_cap' => 4, 'price_per_night' => 800000],
             ['roomtype_name' => 'Junior Suite Room', 'bedsize' => 'M', 'bed_count' =>10, 'max_cap' => 2, 'price_per_night' => 400000],
             ['roomtype_name' => 'Single Room', 'bedsize' => 'S', 'bed_count' =>10, 'max_cap' => 1, 'price_per_night' => 250000],
             // Tambahkan data lain sesuai kebutuhan
