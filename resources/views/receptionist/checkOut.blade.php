4e5c219fc250b74cfe50242d86b193955bac06f9
masten57 <ken.alfa57@gmail.com>
2023-10-26 15:08:54 +0700

    UI CheckIn CheckOut
    Bikin UI tabel check in sama check out

--- resources/views/receptionist/checkOut.blade.php
+++ resources/views/receptionist/checkOut.blade.php
@@ -1,8 +1,9 @@
 @extends('receptionist.app')
 
 @section('body')
-    <div class="d-flex align-items-center justify-content-between">
-        <h1 class="mb-0">List Check Out</h1>
+<div class="flex flex-col items-center justify-center space-y-4 mt-8 font-montserrat">
+    <h1 class="bg-white inline-block rounded font-bold text-center w-[300px] sm:text-lg md:text-xl lg:text-2xl xl:text-3xl"
+    style="box-shadow: 0 4px 0px rgba(220, 66, 149, 1)">List Check Out</h1>
     </div>
     <hr />
     @if (Session::has('success'))
@@ -10,32 +11,33 @@
             {{Session::get('success')}}
         </div>
     @endif
-    <table class="table table-hover">
+    <div class="flex flex-col items-center m-20">
+    <table class="mt-4 bg-white shadow-md rounded-lg overflow-hidden w-full">
         <thead class="table-primary">
             <tr>
-                <th>ID Booking</th>
-                <th>Nama Tamu</th>
-                <th>Tanggal Booking</th>
-                <th>Tanggal Awal</th>
-                <th>Tanggal Selesai</th>
-                <th>Tipe Kamar</th>
-                <th>Harga</th>
-                <th>Check Out</th>
+                <th class="py-2 px-4 text-center">ID Booking</th>
+                <th class="py-2 px-4 text-center">Nama Tamu</th>
+                <th class="py-2 px-4 text-center">Tanggal Booking</th>
+                <th class="py-2 px-4 text-center">Tanggal Awal</th>
+                <th class="py-2 px-4 text-center">Tanggal Selesai</th>
+                <th class="py-2 px-4 text-center">Tipe Kamar</th>
+                <th class="py-2 px-4 text-center">Harga</th>
+                <th class="py-2 px-4 text-center">Check Out</th>
             </tr>
         </thead>
         <tbody>
             @if($checkouttable->count() > 0)
-                @foreach($checkouttable as $bk)
-                    <tr>
-                        <td class="align-middle">{{ $bk->id_booking }}</td>
-                        <td class="align-middle">{{ $bk->guest_name }}</td>
-                        <td class="align-middle">{{ $bk->booking_date }}</td>
-                        <td class="align-middle">{{ $bk->start_date }}</td>
-                        <td class="align-middle">{{ $bk->finish_date }}</td>
-                        <td class="align-middle">{{ $bk->roomtype_name }}</td>
-                        <td class="align-middle">{{ $bk->total_pay }}</td>
-                        <td class="align-middle">
-                            <a href="{{ route('check-out-room', ['roomId' => $bk->id_room]) }}" class="btn btn-primary">Check Out</a>
+                @foreach($checkouttable as $index => $bk)
+                    <tr class="{{ $index % 2 === 0 ? 'bg-pink-100' : 'bg-white' }}">
+                        <td class="py-2 px-4 text-center">{{ $bk->id_booking }}</td>
+                        <td class="py-2 px-4 text-center">{{ $bk->guest_name }}</td>
+                        <td class="py-2 px-4 text-center">{{ $bk->booking_date }}</td>
+                        <td class="py-2 px-4 text-center">{{ $bk->start_date }}</td>
+                        <td class="py-2 px-4 text-center">{{ $bk->finish_date }}</td>
+                        <td class="py-2 px-4 text-center">{{ $bk->roomtype_name }}</td>
+                        <td class="py-2 px-4 text-center">{{ $bk->total_pay }}</td>
+                        <td class="py-2 px-4 text-center">
+                            <a href="{{ route('check-out-room', ['roomId' => $bk->id_room]) }}" class="bg-[#DC4295] hover:bg-pink-700 text-white py-1 px-2 rounded">Check Out</a>
                         </td>
                         </tr>
                 @endforeach
@@ -46,4 +48,5 @@
             @endif
         </tbody>
     </table>
+    </div>
 @endsection
